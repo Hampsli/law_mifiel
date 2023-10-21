dd9f76d756ea503b3faf9ab6b0ec1ad2a72277db
LilianAw <aguirre.wl@gmail.com>
2023-10-21 00:28:25 -0600

    Adding sha256 nodeJs function and two put request to process and send results

--- miniExpress/app.js
+++ miniExpress/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { createHash } = require('crypto');
+
 const path = require('path');
 
 const app = express();
@@ -8,26 +10,29 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', (req, res) => {
-  res.json(`HTTP GET request received`);
-})
-
-
 app.post('/api/v1/users', (req, res) => {
-  console.log(req);
-    let data = req.body;
-    res.send('Data received' + JSON.stringify(data));
-    res.status(200).json({next_challenge: {
-      challenge: 3
+    res.status(200).json({id:1,next_challenge: {
+      challenge: 'Hello, world!'
     } })
+})
+app.post('/api/v1/createSha256/message', (req, res) => {
+  res.status(200).json(createHash('sha256').update(req.body.message).digest('hex'));
 })
 
 app.put('/api/v1/users/:id/challenge/digest', (req, res) => {
+    res.status(200).json({next_challenge: {
+      difficulty: 3,
+      challenge: 'Hello, world!'
+    } })
+})
+
+app.put('/api/v1/users/:id/challenge/pow', (req, res) => {
   console.log(req);
     let data = req.body;
     res.send('Data received' + JSON.stringify(data));
     res.status(200).json({next_challenge: {
-      challenge: 3
+      difficulty: 3,
+      challenge: 'Hello, world!'
     } })
 })
 
--- miniExpress/public/index.js
+++ miniExpress/public/index.js
@@ -1,5 +1,61 @@
 'use strict';
+var resultHistorial = {
+    id:0,
+    next_challenge:{
+     challenge:"",
+     difficulty:0,
+     nonce:"",
+     sha256Message:"",
+     noncePlusSha256:""
+    }
+} ;
+const getDataFromServer = async () => {
+    const firsStepResponse = await fetch("http://localhost:4000/api/v1/users", {
+        method: "POST",
+        body: JSON.stringify({
+           name: "Liliana Aguirre",
+           email: "aguirre.wl@gmail.com"
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      })
+     let firsStepData = await firsStepResponse.json();
+     resultHistorial.id= firsStepData.id;
+     resultHistorial.next_challenge.challenge = firsStepData.next_challenge.challenge
+     hash256Process();
+}
 
-window.onload = (event) => {
-    console.log("page is fully loaded");
-  };
+const putDataSha256= async () =>{
+    const secondStepDataToSend = await fetch(`/api/v1/users/${resultHistorial.id}/challenge/digest`, {
+        method: "PUT",
+        body: JSON.stringify({result: resultHistorial.next_challenge.sha256Message
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      })
+     let secondStepResult = await secondStepDataToSend.json();
+    resultHistorial.next_challenge.difficulty = secondStepResult.next_challenge.difficulty;
+    resultHistorial.next_challenge.challenge = secondStepResult.next_challenge.challenge;
+    pOwProcess();
+}
+
+const hash256Process = async () =>{
+    const secondStepResponse = await fetch(`http://localhost:4000/api/v1/createSha256/message`, {
+        method: "POST",
+        body: JSON.stringify({message: resultHistorial.next_challenge.challenge
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      })
+     let secondStepData = await secondStepResponse.json();
+     resultHistorial.next_challenge.sha256Message = secondStepData;
+     putDataSha256();
+}
+const pOwProcess = async () =>{
+
+}
+document.getElementById("btn").onclick = getDataFromServer();  
+
